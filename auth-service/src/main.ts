81665598b24cde9fd718f02ad4d72a5a640de98d
Adambizien <bizienadam@outlook.com>
2025-11-13 09:28:15 +0100

    test 2

--- auth-service/src/main.ts
+++ auth-service/src/main.ts
@@ -5,6 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   
